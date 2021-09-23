00bb07a3739bb7696076d65672c2c2ebea5cde97
David JULIEN <swy7ch@protonmail.com>
2021-09-23 23:27:41 +0200

    feat: add TS Parsers for latex and verilog

--- .config/nvim/lua/plugin/treesitter.lua
+++ .config/nvim/lua/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-        ensure_installed = { "c", "lua", "python", "bash" },
+        ensure_installed = { "c", "lua", "python", "bash", "latex", "verilog" },
         ignore_install = { "javascript" },
         highlight = {
                 enable = true,
