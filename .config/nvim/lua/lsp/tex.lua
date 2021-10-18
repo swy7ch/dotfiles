ecc15d8798fe62b150da1fd8be94ef7f8e11288e
David JULIEN <swy7ch@protonmail.com>
2021-10-18 22:48:42 +0200

    [nvim] fix: texlab command for lsp
    specified the path to lsp server

--- .config/nvim/lua/lsp/tex.lua
+++ .config/nvim/lua/lsp/tex.lua
@@ -4,6 +4,7 @@
 -- Description   : LaTeX config file for lsp
 
 require("lspconfig").texlab.setup {
+        cmd = { vim.fn.stdpath("data") .. "/lsp_servers/latex/texlab" },
         settings = {
                 build = {
                         executable = "pdflatex"
