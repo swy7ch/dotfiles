d17d90564581a355a46fc5638f66a3f5bff24d18
David JULIEN <swy7ch@protonmail.com>
2021-10-19 19:02:43 +0200

    [nvim] feat: add filetype specific config

---
+++ .config/nvim/after/ftplugin/verilog.lua
@@ -0,0 +1,10 @@
+-- Author       : swytch
+-- Created      : Sunday Oct. 03, 2021 18:06:12 CET
+-- License      : GPLv3
+-- Description  : verilog fietype config
+
+utils.map("n", "<leader>j", ":CoqNext<Enter>")
+utils.map("n", "<leader>k", ":CoqUndo<Enter>")
+utils.map("n", "<leader><Enter>", ":CoqToLine<Enter>")
+
+utils.map("i", "//", "∥")
