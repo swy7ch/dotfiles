9cb79d26786a2d0e9a9ddfe7ae1adfbc64e9d953
David JULIEN <swy7ch@protonmail.com>
2021-10-21 23:47:47 +0200

    [nvim]feat: add maps for verilog

--- .config/nvim/after/ftplugin/verilog.lua
+++ .config/nvim/after/ftplugin/verilog.lua
@@ -3,8 +3,11 @@
 -- License      : GPLv3
 -- Description  : verilog fietype config
 
+utils.map("n", "<C-c>", ":CoqInterrupt<Enter>")
 utils.map("n", "<leader>j", ":CoqNext<Enter>")
 utils.map("n", "<leader>k", ":CoqUndo<Enter>")
 utils.map("n", "<leader><Enter>", ":CoqToLine<Enter>")
+utils.map("n", "<leader>a", ":Coq About")
+utils.map("n", "<leader>G", ":CoqJumpToEnd")
 
 utils.map("i", "//", "∥")
