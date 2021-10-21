e461e3804b11c8f08a5087c464c7c1a7f84c5477
David JULIEN <swy7ch@protonmail.com>
2021-10-21 23:49:39 +0200

    [nvim]fix: verilo maps

--- .config/nvim/after/ftplugin/verilog.lua
+++ .config/nvim/after/ftplugin/verilog.lua
@@ -3,11 +3,11 @@
 -- License      : GPLv3
 -- Description  : verilog fietype config
 
-utils.map("n", "<C-c>", ":CoqInterrupt<Enter>")
-utils.map("n", "<leader>j", ":CoqNext<Enter>")
-utils.map("n", "<leader>k", ":CoqUndo<Enter>")
-utils.map("n", "<leader><Enter>", ":CoqToLine<Enter>")
-utils.map("n", "<leader>a", ":Coq About")
-utils.map("n", "<leader>G", ":CoqJumpToEnd")
+utils.map("n", "<C-c>", "<cmd>CoqInterrupt<CR>")
+utils.map("n", "<leader>j", "<cmd>CoqNext<CR>")
+utils.map("n", "<leader>k", "<cmd>CoqUndo<CR>")
+utils.map("n", "<leader><CR>", "<cmd>CoqToLine<CR>")
+utils.map("n", "<leader>a", "<cmd>Coq About")
+utils.map("n", "<leader>G", "<cmd>CoqJumpToEnd")
 
 utils.map("i", "//", "∥")
